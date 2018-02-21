27f85eeba33cea8bd0a35fcf2b64210982e76d5e
catherineDiaz <catherine.diaz@colorado.edu>
2018-02-20 19:29:51 -0700

    Created Player, Token, Power, UI, and Main objects

---
+++ Assets/Classes/Main.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+
+
+
+
+	void Start () {
+
+		//Player player = new Player("Cat", Vector3(312, 2.5, 238.2));
+		//Power changeToBall = new Power("changeToBall");
+
+
+
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
