c3c35796f406515fbfecafcb60d930b502779296
catherineDiaz <catherine.diaz@colorado.edu>
2018-03-21 23:18:21 -0600

    Updated Scripts and Paint Universe Scene

--- Assets/Classes/PaintUniverse.cs
+++ Assets/Classes/PaintUniverse.cs
@@ -9,21 +9,53 @@ public class PaintUniverse : MonoBehaviour {
 	GameObject personController;
 	GameObject ballFollow;
 	GameObject rollerBall;
+	GameObject paintToken;
+	int once = 0;
 
 
 
-	// Use this for initialization
+	/**
+    	Initializes the Paint Universe object
+
+	*/
 	void Start () {
 
 
 		playerFollow = GameObject.Find("PlayerFollow");
 		ballFollow = GameObject.Find("BallFollow");
 		personController = GameObject.Find("ThirdPersonController");
+		paintToken = GameObject.Find("paintToken");
 
 		
 	}
+
+
+
+	/**
+    	If the token is collected then we launch the UI sending the player back to the home scene
+
+	@params col - the collision that is detected
+
+	*/
+	void OnCollisionEnter (Collision col)
+	{
+		if (gameObject.name == "paintToken" && once == 0) 
+		{
+			once = 1;
+			Debug.Log("Token Touched");
+			UI tokenUIPaint = GameObject.Find("UIController").GetComponent<UI>();
+			tokenUIPaint.TokenUIPaint();
+
+		}
+
+	}
+
 	
-	// Update is called once per frame
+	/**
+		Update the camera following position, if the object is currently the person then have the camera 
+		follow the person, if the person transforms into the ball then follow it 
+
+	*/
 	void Update () {
 		
 		if(personController.activeSelf == false)
